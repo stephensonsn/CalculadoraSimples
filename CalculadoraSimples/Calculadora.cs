85cb6673eb9f0c2c98a038d5974d40acaffc98e6
Stephenson Oliveira <stephenson.silva@outlook.com>
2021-06-03 13:32:53 -0300

    start commit

---
+++ CalculadoraSimples/Calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraSimples {
+    class Calculadora {
+        private double resultadoCalculo;
+
+        public double CalculoSoma(double primeiroNumero, double segundoNumero) {
+            resultadoCalculo = primeiroNumero + segundoNumero;
+            return resultadoCalculo;
+        }
+
+        public double CalculoSubtracao(double primeiroNumero, double segundoNumero) {
+            resultadoCalculo = primeiroNumero - segundoNumero;
+            return resultadoCalculo;
+        }
+
+        public double CalculoMultiplicacao(double primeiroNumero, double segundoNumero) {
+            resultadoCalculo = primeiroNumero * segundoNumero;
+            return resultadoCalculo;
+        }
+
+        public double CalculoDivisao(double primeiroNumero, double segundoNumero) {
+            resultadoCalculo = primeiroNumero / segundoNumero;
+            return resultadoCalculo;
+        }
+
+    }
+}
---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CalculadoraSimples {
+    class Program {
+        static void Main(string[] args) {
+            //2 * (lagura + profundidade) * altura
+            Console.WriteLine("Digite o primeiro número: ");
+            double primeiroNumero = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double segundoNumero = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual tipo de operação deseja fazer?");
+            Console.WriteLine("[1] = Soma");
+            Console.WriteLine("[2] = Subtração");
+            Console.WriteLine("[3] = Multiplicação");
+            Console.WriteLine("[4] = Divisão");
+
+            Calculadora calculadora = new Calculadora();
+
+            switch(Console.ReadLine()) {
+                case "1":
+                    Console.WriteLine("O resultado do seu calculo é: ");
+                    Console.WriteLine(calculadora.CalculoSoma(primeiroNumero, segundoNumero));
+                    break;
+                case "2":
+                    Console.WriteLine("O resultado do seu calculo é: ");
+                    Console.WriteLine(calculadora.CalculoSubtracao(primeiroNumero, segundoNumero));
+                    break;
+                case "3":
+                    Console.WriteLine("O resultado do seu calculo é: ");
+                    Console.WriteLine(calculadora.CalculoMultiplicacao(primeiroNumero, segundoNumero));
+                    break;
+                case "4":
+                    Console.WriteLine("O resultado do seu calculo é: ");
+                    Console.WriteLine(calculadora.CalculoDivisao(primeiroNumero, segundoNumero));
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
